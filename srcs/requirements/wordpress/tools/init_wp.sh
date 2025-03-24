ef433f02600ac64bb397d7bad5b75051cfa424a0
csteudin <therealsolrachan@gmail.com>
2025-03-24 19:30:49 +0100

    init_wp.sh installs wordpress-cli and wordpress now just need to set up WP and connect DB

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 
+mkdir -p /var/www/
+mkdir -p /var/www/html
+cd /var/www/html
+
+rm -rf *
+
+#download this to use wordpress in cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+#move it into bin files so it can be used in cli
+mv wp-cli.phar /usr/local/bin/wp
+
+#download the wp core
+wp core download --allow-root
+
+exec /usr/sbin/php-fpm7.3 -F
