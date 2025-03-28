28ce19aed6c9da969336378f0aa8e67f1d1367c5
john smitht <johnsmiththemith@gmail.com>
2025-03-28 04:43:28 +0100

    pushing new concept of initializing mariadb

--- srcs/requirements/mariadb/tools/init_mariadb.sh
+++ srcs/requirements/mariadb/tools/init_mariadb.sh
@@ -1,34 +1,36 @@
 #!/bin/sh
 
-mysqld &
-export DB_PID=$!
+DB_PASSWORD=$(cat /run/secrets/db_password)
+DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+DB_INITALIZED="/var/lib/mysql/inited"
 
-echo $DB_PID
+if [ ! -d "/var/lib/mysql/mysql"]; then
+	mkdir -p /var/lib/mysql
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 
-while ! mysqladmin ping --silent; do
-	sleep 2
-done
+if [ ! -f "$DB_INITIALIZED"]; then
+	
+	mysql --user=mysql --datadir=/var/lib/mysql &
+	DB_PID=$!
+	
+	while ! mysqladmin ping --silent; do
+		sleep 2
+	done
 
-export DB_PASSWORD=$(cat /run/secrets/db_password)
-export DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+	echo "CREATE DATABASE IF NOT EXISTS $DB_DATABASE ;" > db1.sql
+	echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
+	echo "GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER'@'%' ;" >> db1.sql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD' ;" >> db1.sql
+	echo "FLUSH PRIVILEGES;" >>  db1.sql
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB_DATABASE.* TO '$DB_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >>  
+	mysql < db1.sql
 
-mysql < db1.sql
+	mysqladmin shutdown
+	wait "$DB_PID" || true
 
-#mysql -u root <<EOF
-#ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$DB_ROOT_PASSWORD';
-#FLUSH PRIVILEGES;
-#EOF
+	touch $DB_INITALIZED
+fi
 
-#mysql -u root -p"$DB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE};"
-#mysql -u root -p"$DB_ROOT_PASSWORD" -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-#mysql -u root -p"$DB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON ${DB_DATABASE}.* TO '${DB_USER}'@'%';"
-#mysql -u root -p"$DB_ROOT_PASSWORD" -e "ALTER USER '${DB_ROOT_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-#mysql -u root -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-
-wait
+exec mysql --user=mysql --console
