a0f59fb0dfa95606607a9981657b2b01e17b763a
john smitht <johnsmiththemith@gmail.com>
2025-04-05 18:17:52 +0200

    adding nginx script and cnf file, but isnt working

---
+++ srcs/requirements/nginx/tools/init_nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -f "$SSL_PATH/nginx-selfsigned.crt" ]; then
+    mkdir -p $SSL_PATH
+    openssl req -x509 \
+		-nodes \
+		-days 365 \
+        -newkey rsa:2048 \
+        -keyout "$SSL_PATH/nginx-selfsigned.key" \
+        -out "$SSL_PATH/nginx-selfsigned.crt" \
+        -subj "/C=DE/ST=Baden-Württemberg/L=Heilbronn/O=csteudin/CN=Inception"
+fi
+
+exec nginx -g "daemon off;"
